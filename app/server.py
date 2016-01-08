e2deba2fb52873b8de6966c5bb33de30b4c18020
Brian Hoff <sixthworldrunner@gmail.com>
2016-01-07 16:29:35 -0800

    now the task stored into a map by id the id is used to retrieve the task results from the map this means that the job is run mostly asynchonously from the request

--- app/server.py
+++ app/server.py
@@ -1,7 +1,9 @@
+from uuid import UUID
+
 import flask
 from flask import Flask, request
 
-from calc.entry import calc
+from calc.entry import calc, get_group_results
 
 app = Flask(__name__)
 
@@ -25,10 +27,15 @@ def hi_there(person):
 def print_list():
     data = request.get_json()
     xs = data['list']
-    result = calc.delay(xs)
-    response = result.wait()
+    result = calc(xs)
+    response = result
     return flask.jsonify(response)
 
+
+@app.route('/lists/<task_id>', methods=['GET'])
+def get_task_status_by_id(task_id: UUID):
+    result = get_group_results(task_id)
+    return flask.jsonify(result)
 
 if __name__ == '__main__':
     app.run()
--- calc/entry.py
+++ calc/entry.py
@@ -1,10 +1,18 @@
 from typing import List
 
+from celery import group
+from celery.result import AsyncResult
+
+from uuid import UUID
+
 from calc.runner import runner
 
 from calc.sum import summation
 from calc.count import count
 from calc.avg import average
+
+submitted_tasks = {}
+
 
 # yes this is absurd
 # its more about proof of concept than actually
@@ -12,10 +20,35 @@ from calc.avg import average
 # this should really send the results to a db
 # or some other form of persistent storage
 @runner.task
-def calc(xs: List[float]):
-    summ = summation.delay(xs).wait()
-    cnt = count.delay(xs).wait()
-    avg = average.delay(xs).wait()
-    return dict(avg=avg,
-                sum=summ,
-                count=cnt)
+def calc(xs: List[float]) -> UUID:
+    tasks = group([summation.s(xs), count.s(xs), average.s(xs)])
+    calculations = tasks.apply_async()
+    task_id = calculations.id
+
+    global submitted_tasks
+    submitted_tasks[task_id] = calculations
+
+    return dict(uuid=task_id)
+
+
+def get_group_results(task_id: UUID):
+    global submitted_tasks
+    task = submitted_tasks[task_id]
+
+    if task is None:
+        return dict(msg='No such id')
+    else:
+        return dict(sum=get_subtask_result(task.children[0]),
+                    count=get_subtask_result(task.children[1]),
+                    avg=get_subtask_result(task.children[2]))
+
+
+def get_subtask_result(async_result: AsyncResult):
+    """
+
+    :type async_result: AsyncResult
+    """
+    if async_result.successful():
+        return async_result.get()
+    else:
+        return async_result.status
