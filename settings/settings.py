dad10b0ca5b2e7d311527df89827b52e181f6dd8
Jordan Avery <jordan.avery1@gmail.com>
2016-01-27 15:02:25 -0800

    Remove DB settings from Lintball.

--- settings/settings.py
+++ settings/settings.py
@@ -18,12 +18,5 @@ LINTBALL_SETTINGS = {
     'celery': {
         'BROKER': os.environ.get('CELERY_BROKER', 'amqp://'),
         'BACKEND': os.environ.get('CELERY_BACKEND', 'redis://')
-    },
-
-    'peewee': {
-        'HOST': os.environ.get('LINTBALL_DB_HOST', 'localhost'),
-        'PORT': os.environ.get('LINTBALL_DB_PORT', '1234'),
-        'USER': os.environ.get('LINTBALL_DB_USER', ''),
-        'PASS': os.environ.get("LINTBALL_DB_PASS", '')
     }
 }
