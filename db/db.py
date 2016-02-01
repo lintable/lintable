f90fc03971cde45140f27fc25e0915fc687ca77b
Ian Copp <copp.ian@gmail.com>
2016-01-31 23:33:08 -0800

    Add initial database hooks to the web stuff

--- db/db.py
+++ db/db.py
@@ -60,27 +60,48 @@ class Jobs(BaseModel):
     commentNumber = IntegerField()
     status = CharField()
 
-
-def get_user(username: str) -> User:
+def get_user(identifier: Union[int, str]) -> User:
     """
-    Finds a user for a given user name
+    Finds a user for a given ID or username.
 
-    :param username:
+    :param identifier:
     :return User or None:
     """
     try:
-        user = User.get(User.username == username)
+        if isinstance(identifier, int):
+            user = User.get(User.id == identifier)
+        if isinstance(identifier, str):
+            user = User.get(User.username == identifier)
     except User.DoesNotExist as e:
         user = None
         logger.error(
-            'No user found for user name: {0}.\nException:\n\t{1}'.format(
-                username, e))
+            'No user found for identifier: {0}.\nException:\n\t{1}'.format(
+                identifier, e))
     except Exception as e:
         user = None
         logger.error(e)
 
     return user
 
+def get_job(identifier: int) -> Jobs:
+    """
+    Finds a job for a given job ID.
+
+    :param identifier:
+    :return Jobs or None:
+    """
+    try:
+        job = Jobs.get(Jobs.jobId == identifier)
+    except Jobs.DoesNotExist as e:
+        job = None
+        logger.error(
+            'No job found for identifier: {0}.\nException:\n\t{1}'.format(
+                identifier, e))
+    except Exception as e:
+        job = None
+        logger.error(e)
+
+    return job
 
 def get_repo(url: str) -> Repo:
     """
@@ -100,22 +121,46 @@ def get_repo(url: str) -> Repo:
         logger.error(e)
     return repo
 
-
-def get_jobs_for_user(username: str) -> Iterable[Jobs]:
+def get_repos_for_user(identifier: Union[int, str]) -> Iterable[Repo]:
     """
-    Finds any jobs currently in the system for a given user name.
+    Finds any repos associated with a given username or user ID.
 
-    :param username:
+    :param identifier:
+    :return Iterable object of Repo or None:
+    """
+    try:
+        if isinstance(identifier, int):
+            repos = SelectQuery(Repo).join(User).where(User.id == Repo.id,
+                                                       User.id == identifier)
+        if isinstance(identifier, str):
+            repos = SelectQuery(Repo).join(User).where(User.id == Repo.id,
+                                                       User.username == identifier)
+    except Exception as e:
+        repos = None
+        logger.error(
+            'No repos found for user with identifier:{0}.\nException:\n\t{'
+            '1}'.format(identifier, e))
+    return repos
+
+def get_jobs_for_user(identifier: Union[int, str]) -> Iterable[Jobs]:
+    """
+    Finds any jobs currently in the system for a given username or user ID.
+
+    :param identifier:
     :return Iterable object of Jobs or None:
     """
     try:
-        jobs = SelectQuery(Jobs).join(User).where(User.id == Jobs.id,
-                                                  User.username == username)
+        if isinstance(identifier, int):
+            jobs = SelectQuery(Jobs).join(User).where(User.id == Jobs.id,
+                                                      User.id == identifier)
+        if isinstance(identifier, str):
+            jobs = SelectQuery(Jobs).join(User).where(User.id == Jobs.id,
+                                                      User.username == identifier)
     except Exception as e:
         jobs = None
         logger.error(
-            'No jobs found for user with username:{0}.\nException:\n\t{'
-            '1}'.format(username, e))
+            'No jobs found for user with identifier:{0}.\nException:\n\t{'
+            '1}'.format(identifier, e))
     return jobs
 
 
--- lintweb/views.py
+++ lintweb/views.py
@@ -1,6 +1,8 @@
 """Supply the frontend pages."""
 
+from datetime import datetime
 from flask import request, render_template
+from db import db
 from lintweb import app
 # from lintball.lintball import lint_github
 
@@ -19,10 +21,21 @@ if not DEBUG:
         return render_template('index.html')
 
     @app.route('/account')
-    @app.route('/account/<accountid>')
-    def account(accountid=None):
+    @app.route('/account/<identifier>')
+    def account(identifier=None):
         """View details for an existing user account."""
-        return render_template('account.html', accountid=accountid)
+        # TODO: Use logged in cookies to get user automatically
+        # TODO: Only allowing viewing a user page for a logged in user
+        #       (in the future we can extend this to an admin viewing any user)
+        if identifier is not None:
+            user = db.get_user(identifier)
+            repos = db.get_repos_for_user(identifier)
+            return render_template('account.html',
+                                   username=user.username,
+                                   repos=repos)
+        else:
+            user = None
+            return render_template('account.html')
 
     @app.route('/register')
     def register():
@@ -35,14 +48,30 @@ if not DEBUG:
         return render_template('login.html')
 
     @app.route('/status')
-    @app.route('/status/<jobid>')
-    def status(jobid=None):
-        """Get the status of a given linting job in progress."""
-        return render_template('status.html', jobid=jobid)
+    @app.route('/status/<identifier>')
+    def status(identifier=None):
+        """Get a list of active jobs, or get the status of a job in progress."""
+        if identifier is not None:
+            job = db.get_job(identifier)
+            if not job.endTime:
+                currently_running = True
+                duration = datetime.now() - job.startTime
+            else:
+                currently_running = False
+                duration = job.endTime - job.startTime
+            return render_template('status.html',
+                                   identifier=job.id,
+                                   repo_url=job.url,
+                                   currently_running=currently_running,
+                                   duration=duration,
+                                   status=job.status)
+        else:
+            # TODO: Use logged in cookies to get jobs for user
+            return render_template('status.html')
 
     @app.route('/terms')
     def terms():
-        """View the terms of service.."""
+        """View the terms of service."""
         return render_template('terms.html')
 
     @app.route('/privacy')
