dfa08542ac5857d20391479c9e58dfcb6aac7a0d
Brian Hoff <sixthworldrunner@gmail.com>
2016-02-09 13:39:24 -0800

    pulled in updates for the flask webserver from Ian's PyLint branch
    he pulled the routes into __init__.py and tweaked the test_views

--- lintable_web/test_views.py
+++ lintable_web/test_views.py
@@ -1,3 +1,5 @@
+"""Tests for the lintable_web views."""
+
 # Copyright 2015-2016 Capstone Team G
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,18 +20,28 @@ from lintable_settings.settings import LINTWEB_SETTINGS
 from lintable_web import app
 
 class ViewsTests(unittest.TestCase):
+    """Tests for the lintable_web views."""
+
     def setUp(self):
+        """Set up the tests."""
+
         self.app = app.test_client()
         self.app.testing = True
 
     def tearDown(self):
+        """Finish the tests."""
+
         pass
 
     def test_index_status_code(self):
+        """Make sure that the homepage returns status 200."""
+
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
 
     def test_register_status_code(self):
+        """Make sure that the register page returns status 302 or 404."""
+
         result = self.app.get('/register')
 
         if not LINTWEB_SETTINGS['DEBUG']:
@@ -37,7 +49,9 @@ class ViewsTests(unittest.TestCase):
         else:
             self.assertEqual(result.status_code, 404)
 
-    def test_index_status_code(self):
+    def test_callback_status_code(self):
+        """Make sure that the callback page returns status 302 or 404."""
+
         result = self.app.get('/callback')
 
         if not LINTWEB_SETTINGS['DEBUG']:
