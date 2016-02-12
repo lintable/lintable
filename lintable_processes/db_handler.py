8f29bd30d03268e72af2eccf24a47db378668eb7
Brian Hoff <sixthworldrunner@gmail.com>
2016-02-11 20:38:44 -0800

    fixed bugs in db_handler
    added notice text
    changed repo_id type from str to int
    changed selection of repo_fk.repo_owner to owner_id
    changed status text in clone_repo from CLONED to CLONE_REPO

--- lintable_processes/db_handler.py
+++ lintable_processes/db_handler.py
@@ -1,3 +1,17 @@
+# Copyright 2015-2016 Capstone Team G
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Optional
 from uuid import UUID
 
@@ -13,7 +27,7 @@ from lintable_processes.do_nothing_handler import DoNothingHandler
 
 class DBHandler(DoNothingHandler):
 
-    def __init__(self, repo_id: str):
+    def __init__(self, repo_id: int):
         super().__init__()
         self.job = None  # type: Optional[Jobs]
         self.repo_id = repo_id
@@ -26,9 +40,9 @@ class DBHandler(DoNothingHandler):
 
     def started(self, uuid: UUID, comment_id: int = None):
         super().started(uuid, comment_id)
-        self.repo_fk = DatabaseHandler.get_repo(self.repo_id)
+        self.repo_fk = DatabaseHandler.get_repo(identifier=self.repo_id)
         self.job = Jobs.create(job_id=uuid,
-                               repo_owner=self.repo_fk.repo_owner,
+                               repo_owner=self.repo_fk.owner_id,
                                repo=self.repo_fk,
                                start_time=datetime.datetime.now(),
                                end_time=None,
@@ -58,5 +72,5 @@ class DBHandler(DoNothingHandler):
 
     def clone_repo(self, uuid: UUID, repo: Repo, local_path: str):
         super().clone_repo(uuid, repo, local_path)
-        self.job.status = 'CLONED'
+        self.job.status = 'CLONE_REPO'
         self.job.save()
