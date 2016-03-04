e130a4bf6f6a83fab8d8e2e2821867c5e294b2ae
Brian Hoff <sixthworldrunner@gmail.com>
2016-03-03 23:00:29 -0800

    added missing template parameter

--- lintable_web/__init__.py
+++ lintable_web/__init__.py
@@ -213,7 +213,7 @@ if not DEBUG:
             LOGGER.error('full_name: {full_name}\t\twebhook?: {webhook}'.format(full_name=repo['full_name'],
                                                                                 webhook=repo['webhook']))
 
-        return render_template('list_repos.html', repos=repos)
+        return render_template('list_repos.html', current_user=current_user, repos=repos)
 
 
     @app.route('/logout')
